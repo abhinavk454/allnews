5302b9e02efaa3045e9fec3898560ec3b58e8feb
Abhinav Kumar <32016468+abhinavk454@users.noreply.github.com>
2020-07-26 11:48:07 +0530

    done imports for Nav and App.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
+import Nav from './components/Nav';
 function App() {
   return (
     <div>
-
+      <Nav />
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
+import Entertainment from '../pages/Entertainment';
+import Home from '../pages/Home';
+import Politics from '../pages/Politics';
+import Sport from '../pages/Sports';
 
 function Nav() {
     return (
