a3975c8d6fe053fe018f45523bb879c6091cedd8
Abhinav Kumar <32016468+abhinavk454@users.noreply.github.com>
2020-07-26 14:34:16 +0530

    done Home Politics and Sports section.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,19 +3,22 @@ import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
 import Entertainment from '../pages/Entertainment';
 import Home from '../pages/Home';
 import Politics from '../pages/Politics';
-import Sport from '../pages/Sports';
+import Sports from '../pages/Sports';
+import Search from '../pages/Search';
 
 function Nav() {
     return (
         <Router>
-            <Link to="/home">To Home</Link>
+            <Link to='/home'>Home</Link>
+            <Link to="/top-headlines">Top Headlines</Link>
             <Link to="/sports">To Sports</Link>
             <Link to="/entertainment">To Entertainment</Link>
             <Link to='/politics'>To Politics</Link>
             <Switch>
-                <Route exact path='/' component={Home}/>
+                <Route exact path='/home' component={Search}/>
+                <Route path='/top-headlines' component={Home}/>
                 <Route path='/home' component={Home}/>
-                <Route path='/sports' component={Sport}/>
+                <Route path='/sports' component={Sports}/>
                 <Route path='/entertainment' component={Entertainment}/>
                 <Route path='/politics' component={Politics}/>
             </Switch>
---
+++ src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div>
+            <form>
+                <text type=""></text>
+            </form>
+        </div>
+    )
+}
+
+export default Search
