3b60f5cfcc8d8e05612c38aab7f82e04812be4d4
Abhinav Kumar <32016468+abhinavk454@users.noreply.github.com>
2020-07-26 14:49:36 +0530

    done Search section.

---
+++ src/components/HandleSearch.jsx
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+
+function HandleSearch({query}) {
+    const [news,setNews]=useState([]);
+    useEffect(()=>{
+        axios
+        .get(`https://newsapi.org/v2/everything?q=${query}&apiKey=498ea298d85448b0b4efb9d95ffb002f`)
+        .then((res)=>{
+            setNews(res.data.articles)
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
+    },[query])
+    return (
+        <div>
+            {
+                news.map((re,index)=>(<div className="inner" key={index}>
+                <article id="art" key={re.source.id}>
+                  <img id="img" src={re.urlToImage} alt="" />
+                  <br />
+                  <span id="sp1">Title : {re.title}</span>
+                  <br />
+                  Author : {re.author}
+                  <br />
+                  Content : {re.content}
+                  <br />
+                  <a href={re.url} target="_blank">
+                    For more
+                  </a>
+                  <br />
+                  <span>By </span>: {re.source.name}
+                  <br />
+                  <span>Date </span>: {re.publishedAt}
+                </article>
+              </div>))
+            }
+        </div>
+    )
+}
+
+export default HandleSearch
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
+import HandleSearch from '../components/HandleSearch'
 
 function Search() {
+    const [ques,setQues]=useState("");
     return (
         <div>
             <form>
-                <text type=""></text>
+                <input 
+                    type="text" 
+                    value={ques} 
+                    onChange={(e)=>{
+                        setQues(e.target.value)
+                    }}
+                />
             </form>
+            <div>
+                <HandleSearch />
+            </div>
         </div>
     )
 }
