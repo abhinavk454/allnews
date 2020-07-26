009dda0901d385bf7d1f680ca6f8a6194796620b
Abhinav Kumar <32016468+abhinavk454@users.noreply.github.com>
2020-07-26 11:21:10 +0530

    done template making.

---
+++ src/pages/Politics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Politics() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Politics
---
+++ src/pages/Sports.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sports() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Sports
